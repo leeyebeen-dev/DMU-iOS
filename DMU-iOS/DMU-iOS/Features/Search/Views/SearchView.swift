29a04237c8abf17a4ad68c57388357ae5486d170
leeyebeen-dev <lyb0201190@gmail.com>
2024-04-03 10:14:26 +0900

    [CHORE/#109] 검색바 여백 수정

--- DMU-iOS/DMU-iOS/Features/Search/Views/SearchView.swift
+++ DMU-iOS/DMU-iOS/Features/Search/Views/SearchView.swift
@@ -97,7 +97,7 @@ struct SearchBarView: View {
                     }
                 }
             })
-            .padding(EdgeInsets(top: 12, leading: 40, bottom: 12, trailing: 12))
+            .padding(EdgeInsets(top: 12, leading: 40, bottom: 12, trailing: 36))
             .background(textfieldBackgroundColor)
             .foregroundColor(textfieldForegroundColor)
             .font(.Medium16)
@@ -108,6 +108,7 @@ struct SearchBarView: View {
                     Image(systemName: "magnifyingglass")
                         .foregroundColor(imageForegroundColor)
                         .padding(.leading, 12)
+                        .padding(.trailing, 12)
 
                     Spacer()
 
