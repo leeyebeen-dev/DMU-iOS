33d4f5d78844813fe3efd0320e099fcd35eab8ae
leeyebeen-dev <lyb0201190@gmail.com>
2024-09-13 09:10:32 +0900

    [FIX/#120] 일품 데이터 구조 변경

--- DMU-iOS/DMU-iOS/Features/Meal/Models/DTO/Menu.swift
+++ DMU-iOS/DMU-iOS/Features/Meal/Models/DTO/Menu.swift
@@ -16,11 +16,25 @@ struct Menu: Identifiable {
 }
 
 // MARK: - 일품
+
+enum Weekday: String, CaseIterable {
+    case monday = "월요일"
+    case tuesday = "화요일"
+    case wednesday = "수요일"
+    case thursday = "목요일"
+    case friday = "금요일"
+}
+
 struct OneMenu: Identifiable {
     var id = UUID()
     var details: [String]
+    var availableDays: [Weekday]
 }
 
 let OneMenuList = [
-    OneMenu(details: ["라면 3,500원", "치즈라면 4,000원", "해물라면 4,500원", "돈까스 5,000원", "치즈돈까스 5,500원", "고구마치즈돈까스 6,000원", "스팸김치볶음밥 4,900원", "치킨마요덮밥 4,500원", "불닭마요덮밥 4,500원", "오므라이스 5,500원"]),
+    OneMenu(details: ["라면 3,500원", "치즈라면 4,000원", "해물라면 4,500원"], availableDays: [.monday, .tuesday, .wednesday, .thursday, .friday]),
+    OneMenu(details: ["돈까스 5,000원", "치즈돈까스 5,500원", "고구마치즈돈까스 6,000원"], availableDays: [.monday, .tuesday, .wednesday, .thursday, .friday]),
+    OneMenu(details: ["스팸김치볶음밥 4,900원"], availableDays: [.monday, .tuesday]),
+    OneMenu(details: ["치킨마요덮밥 4,500원", "불닭마요덮밥 4,500원"], availableDays: [.wednesday, .thursday]),
+    OneMenu(details: ["오므라이스 5,500원"], availableDays: [.friday]),
 ]
